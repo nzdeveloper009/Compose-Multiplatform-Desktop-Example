a47a0c5b01ecffe3ee82cbc3780a4a38b6c2736c
Syed Nokhaiz Al Hassan <nzdeveloper009@gmail.com>
2024-05-27 17:06:14 +0500

    MouseHoverContent_DraggableContent

--- composeApp/src/desktopMain/kotlin/screen/ExampleScreen.kt
+++ composeApp/src/desktopMain/kotlin/screen/ExampleScreen.kt
@@ -1,12 +1,16 @@
 package screen
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.HorizontalScrollbar
+import androidx.compose.foundation.PointerMatcher
 import androidx.compose.foundation.TooltipArea
 import androidx.compose.foundation.TooltipPlacement
 import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,7 +20,9 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
@@ -33,8 +39,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEvent
@@ -42,6 +50,11 @@ import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.input.key.type
+import androidx.compose.ui.input.pointer.PointerEvent
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
@@ -49,7 +62,7 @@ import cafe.adriel.voyager.core.screen.Screen
 class ExampleScreen : Screen {
     @Composable
     override fun Content() {
-        ClickEventContent()
+        DraggableContent()
     }
 }
 
@@ -238,5 +251,61 @@ fun ClickEventContent(modifier: Modifier = Modifier) {
                 fontSize = 40.sp
             )
         }
+    }
+}
+
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun MouseHoverContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.background(Color.White),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        repeat(10) { index ->
+            var hovered by remember {
+                mutableStateOf(false)
+            }
+            val animatedColor by animateColorAsState(
+                targetValue = if (hovered) Color.Cyan else Color.White,
+                animationSpec = tween(200)
+            )
+            Text(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .background(color = animatedColor)
+                    .padding(all = 8.dp)
+                    .onPointerEvent(PointerEventType.Enter) { hovered = true }
+                    .onPointerEvent(PointerEventType.Exit) { hovered = false },
+                fontSize = 30.sp,
+                text = "Item with the number: $index"
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun DraggableContent(modifier: Modifier = Modifier) {
+    var topBoxOffset by remember { mutableStateOf(Offset(0f, 0f)) }
+    Box(
+        modifier = modifier
+            .size(100.dp)
+            .offset {
+                IntOffset(topBoxOffset.x.toInt(), topBoxOffset.y.toInt())
+            }
+            .background(Color.Blue)
+            .pointerInput(Unit) {
+                detectDragGestures(matcher = PointerMatcher.Primary) {
+                    topBoxOffset += it
+                }
+            }
+    ) {
+        Text(
+            modifier = modifier
+                .align(Alignment.Center),
+            text = "Draggable",
+            color = Color.White
+        )
     }
 }
